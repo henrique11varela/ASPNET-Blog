4b7c27cac6ad6831a8564f74cb4974c51a5b1cd1
PedroNito <Pedro.nito.23@gmail.com>
2023-11-10 22:41:02 +0000

    Navbar done

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,14 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-localStorage.setItem("theme", "")
-const button = document.getElementById("themeToggler")
+const button = document.getElementById("theme-toggle")
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 button.addEventListener("click", () => {
     if (localStorage.getItem("theme") != "dark") {
         document.documentElement.classList.add("dark");
+        themeToggleDarkIcon.classList.add('hidden');
+        themeToggleLightIcon.classList.remove('hidden');
         localStorage.setItem("theme", "dark")
     } else {
         document.documentElement.classList.remove("dark");
+        themeToggleDarkIcon.classList.remove('hidden');
+        themeToggleLightIcon.classList.add('hidden');
         localStorage.setItem("theme", "")
     }
 })
+if (localStorage.getItem("theme") != "dark") {
+    document.documentElement.classList.remove("dark");
+    themeToggleDarkIcon.classList.remove('hidden');
+    themeToggleLightIcon.classList.add('hidden');
+} else {
+    document.documentElement.classList.add("dark");
+    themeToggleDarkIcon.classList.add('hidden');
+    themeToggleLightIcon.classList.remove('hidden');
+}
