63f4062fd827b9eaeb49f4de8cd2e679bcfc63be
henrique11varela <henrique11varela@gmail.com>
2023-11-10 22:08:40 +0000

    post tw done

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+localStorage.setItem("theme", "")
+const button = document.getElementById("themeToggler")
+button.addEventListener("click", () => {
+    if (localStorage.getItem("theme") != "dark") {
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark")
+    } else {
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "")
+    }
+})
